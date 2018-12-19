728b77460c8f17b6f0eb97dd1800f245b621c4e1
P. I. Harrington <71712.2415@compuserve.com>
2018-12-18 19:36:26 -0600

    Modifiable OI

--- PIH G15 cmd based/src/OI.cpp
+++ PIH G15 cmd based/src/OI.cpp
@@ -10,23 +10,47 @@
 #include <WPILib.h>
 # include "Robot.h"
 
-class GearShiftCommand:public Command{
-	DriveSystem::GearState gear;
+class GearShiftCommand: public Command {
 
 public:
-	GearShiftCommand(DriveSystem::GearState togear) : gear(togear)
-		{Requires(&Robot::driveSystem);}
 
-	void Initialize() override {Robot::driveSystem.shiftgears(gear);}
+	void Initialize() override {Robot::driveSystem.shiftgears(DriveSystem::High);}
 //	void Execute() override;
-	bool IsFinished() override {return true;}
-//	void End() override;
+	bool IsFinished() override {return false;}
+	void End() override {Robot::driveSystem.shiftgears(DriveSystem::Low);}
 //	void Interrupted() override;
 
-};
+} gearShiftCommand;
+
+struct TwoGearBtns: Command {
+	bool IsFinished() {return false;}
+	void Initialize() {OI::isTwoBtn = true;}
+	void End() {OI::isTwoBtn = false;}
+} twoGearBtns;
+
+/* Notes for future OI:
+ * Once a command is assigned to a button-state, it cannot be removed.
+ * More than one command can be assigned to the same button-state.
+ * One command can be assigned to more than one button-state.
+ * One can make a command's behavior depend on an outside variable.
+ * One can define a button or trigger whose responses depend on an outside variable.
+ */
 
 OI::OI() {
-	HighGearBtn.WhenPressed(new GearShiftCommand(DriveSystem::High));
-	LowGearBtn.WhenPressed(new GearShiftCommand(DriveSystem::Low));
+	HighGearBtn.WhenPressed(&gearShiftCommand);
+	LowGearBtn.CancelWhenPressed(&gearShiftCommand);
+	ToggleGearBtn.ToggleWhenPressed(&gearShiftCommand);
+	frc::SmartDashboard::PutData(&gearShiftCommand);
+	frc::SmartDashboard::PutData(&twoGearBtns);
 	// Process operator interface input here.
+}
+
+bool OI::isTwoBtn = false;
+
+inline bool OI::TwoBtn() {
+	return isTwoBtn;
+}
+
+inline bool OI::OneBtn() {
+	return !isTwoBtn;
 }
--- PIH G15 cmd based/src/OI.h
+++ PIH G15 cmd based/src/OI.h
@@ -9,10 +9,24 @@
 #include <Joystick.h>
 #include <Buttons/JoystickButton.h>
 
+struct MaybeJButton : frc::JoystickButton {
+	MaybeJButton (GenericHID* joystick, int buttonNumber, bool condition()) :
+		frc::JoystickButton (joystick, buttonNumber), cond(condition) {}
+	bool Get() {
+		return cond() && frc::JoystickButton::Get();
+	}
+private:
+	bool (*cond)();
+};
+
+
 class OI {
+static bool isTwoBtn, OneBtn(), TwoBtn();
+friend struct TwoGearBtns;
 public:
 	Joystick stick{0};
-	JoystickButton HighGearBtn{&stick, 4};
-	JoystickButton LowGearBtn{&stick, 2};
+	MaybeJButton HighGearBtn{&stick, 4, TwoBtn};
+	MaybeJButton LowGearBtn{&stick, 1, TwoBtn};
+	MaybeJButton ToggleGearBtn{&stick, 4, OneBtn};
 	OI();
 };
